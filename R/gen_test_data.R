4d4ace74eb7808e2eebe87a49f3f2360be3a968a
Richard Munoz <rmunoz@cs.columbia.edu>
2015-11-18 10:18:35 -0500

    save data to subdirectory

--- R/gen_test_data.R
+++ R/gen_test_data.R
@@ -1,12 +1,12 @@
-# gen_test_data.R 
+# gen_test_data.R
 # Test data for chrome.R
 # Copyright (c) 2015 Richard Munoz
 #
 # This file is part of chromeR.
 #
 # chromeR is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software 
-# Foundation, either version 3 of the License, or (at your option) any later 
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
 # chromeR is distributed in the hope that it will be useful, but WITHOUT ANY
@@ -18,10 +18,10 @@
 
 # Chromosome lengths per
 # https://en.wikipedia.org/wiki/Human_genome
-chrome_bp <- c(249250621, 243199373, 198022430, 191154276, 180915260, 
+chrome_bp <- c(249250621, 243199373, 198022430, 191154276, 180915260,
               171115067, 159138663, 146364022, 141213431, 135534747,
-              135006516, 133851895, 115169878, 107349540, 102531392, 
-              90354753,  81195210,  78077248,  59128983,  63025520, 
+              135006516, 133851895, 115169878, 107349540, 102531392,
+              90354753,  81195210,  78077248,  59128983,  63025520,
               48129895,  51304566)
 c <- chrome_bp
 
@@ -50,14 +50,14 @@ p2 <- data.frame(result_id=rep(2), indv1=rep("A"), indv2=rep("C"),
 
 # Two segments, gap of 2e5
 p3 <- data.frame(result_id=rep(3), indv1=rep("A"), indv2=rep("D"),
-                 chromosome=10, 
-                 bp_start=c(c[10]/2, c[10]/2+1e7+2e5), 
+                 chromosome=10,
+                 bp_start=c(c[10]/2, c[10]/2+1e7+2e5),
                  bp_end=c(c[10]/2+1e7, c[10]/2+1e7+2e5+2e7),
                  length=runif(2), stringsAsFactors=FALSE)
 
 # Share segments at beginning, middle, and end of chromosomes
 p4 <- data.frame(result_id=rep(4), indv1=rep("A"), indv2=rep("E"),
-                 chromosome=c(1, 2, 3, 4, 4, 4), 
+                 chromosome=c(1, 2, 3, 4, 4, 4),
                  bp_start=c(0, 1, c[3]*(4/5), 0, c[4]/5, c[4]*(5/6)),
                  bp_end=c(c[1]/2, c[2]/5, c[3], c[4]/5-2e5, c[4]/5+1e6, c[4]),
                  length=runif(6), stringsAsFactors=FALSE)
@@ -66,4 +66,4 @@ p4 <- data.frame(result_id=rep(4), indv1=rep("A"), indv2=rep("E"),
 df <- rbind(p1, p2, p3, p4)
 df <- data.frame(id=seq(dim(df)[1]), df)
 test_ibd_segments <- df
-save(test_ibd_segments, chrome_bp, file="chromeR.RData")
+save(test_ibd_segments, chrome_bp, file="data/chromeR.RData")
